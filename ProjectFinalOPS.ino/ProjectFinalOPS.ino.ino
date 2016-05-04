73e08eb97526b3f45ff1eedc59d6febffce1441c
Vansh Gandhi <vansh.gandhi@gmail.com>
2016-05-03 17:59:16 -0700

    Code is better now

--- ProjectFinalOPS.ino/ProjectFinalOPS.ino.ino
+++ ProjectFinalOPS.ino/ProjectFinalOPS.ino.ino
@@ -5,8 +5,8 @@
 #define IN2 17
 #define IN3 6
 #define IN4 5
-#define PWM_L 3
-#define PWM_R 22
+#define PWM_L 22
+#define PWM_R 3
 
 int zeroPCB1 = 0;
 int zeroPCB2 = 0;
@@ -31,8 +31,8 @@ int tolerance;
 
 int time;  // Time between control loop iterations
 int integral;
-double kp; // Proportional gain
-double ki; // Integral gain
+double kp = 5; // Proportional gain
+double ki = 1; // Integral gain
 
 // Functions to move and decide how to move
 int acquireSensor(int pin);
@@ -43,7 +43,7 @@ void stopMoving();
 bool wallsPresent();
 void turn();
 
-int threshold;
+int threshold_R, threshold_L;
 
 void setup() {
   Serial.begin(9600);
@@ -69,38 +69,46 @@ void setup() {
   zeroPCB2 /= zeroConstant;
   zeroPCBFront /= zeroConstant;
 
-  threshold = zeroPCB1;
-  Serial.println(threshold);
-  kp = 1;
-  ki = 1;
+  threshold_L = zeroPCB1;
+  threshold_R = zeroPCB2;
+  Serial.println(threshold_L);
+  Serial.println(threshold_R);
   time = 100;
-
+  rightError = 0;
+  leftError = 0;
+  totalError = 0;
+  prevRightError = 0;
+  prevLeftError = 0;
   //TODO: initialize proportional and integral error  
 }
 
 void loop() {
+  Serial.print("Right: ");
+  Serial.println(acquireSensor(PCB_R));
+  Serial.print("Left: ");
+  Serial.println(acquireSensor(PCB_L)); 
   while(wallsPresent()) {
     moveStraight();
   }
   stopMoving();
   turn();
-  Serial.println(analogRead(PCB_L));
   delay(time);
 }
 
 bool wallsPresent() {
-  //Checks if rodent can move forward
   int right = acquireSensor(PCB_R);
   int left = acquireSensor(PCB_L);
   int front = acquireSensor(PCB_F);
-//  int threshold = 500; //TODO: Find the threshold
-return true;
-//  return (right > threshold-100 && left > threshold-100);
+  if (left < threshold_L-100)
+    return false;
+  if (right < threshold_R-100)
+    return false;
+  return true;
 }
 
 void moveStraight() {
-  rightDistance = acquireSensor(PCB_L); //TODO: Should this be analogRead or acquireSensor?
-  leftDistance = acquireSensor(PCB_R);
+  rightDistance = acquireSensor(PCB_R);
+  leftDistance = acquireSensor(PCB_L);
 
   rightError = rightBaseline - rightDistance;
   leftError = leftBaseline - leftDistance;
@@ -129,12 +137,13 @@ void moveStraight() {
     leftSpeed = 255;
   }
 
-  digitalWrite(IN1, HIGH); //TODO: MIGHT NEED TO SWITCH THE HIGH/LOW
-  digitalWrite(IN2, LOW); 
-  digitalWrite(IN3, HIGH); //TODO: MIGHT NEED TO SWITCH THE HIGH/LOW
-  digitalWrite(IN4, LOW);
+  digitalWrite(IN1, LOW); //THIS MOVES FORWARD CORRECTLY. THE HIGH AND LOWS ARE CORRECT
+  digitalWrite(IN2, HIGH); 
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
   analogWrite(PWM_R, rightSpeed);
-  analogWrite(PWM_R, leftSpeed);
+  analogWrite(PWM_L, leftSpeed);
   
 }
 
@@ -147,32 +156,52 @@ void stopMoving() {
 
 void turn() {
     //figures out which direction to turn, and then EXECUTES
-    
-}
-
-void turnRight() {
-  const int turnTime = 250;
-  digitalWrite(IN1, HIGH); //TODO: MIGHT NEED TO SWITCH THE HIGH/LOW
-  digitalWrite(IN2, LOW); 
-  digitalWrite(IN3, LOW); //TODO: MIGHT NEED TO SWITCH THE HIGH/LOW
-  digitalWrite(IN4, HIGH);
-  delay(turnTime);
-  moveStraight();
+    int right = acquireSensor(PCB_R);
+    int left = acquireSensor(PCB_L);
+    if (right < threshold_R-100) {
+      turnRight();
+    } else if (left < threshold_L-100) {
+      turnLeft();
+    }
 }
 
 void turnLeft() {
   const int turnTime = 250;
-  digitalWrite(IN1, LOW); //TODO: MIGHT NEED TO SWITCH THE HIGH/LOW
-  digitalWrite(IN2, HIGH); 
-  digitalWrite(IN3, HIGH); //TODO: MIGHT NEED TO SWITCH THE HIGH/LOW
-  digitalWrite(IN4, LOW);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW); 
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(PWM_R, 200);
+  analogWrite(PWM_L, 200);
   delay(turnTime);
-  moveStraight();
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW); 
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(PWM_R, 0);
+  analogWrite(PWM_L, 0);
+}
+
+void turnRight() {
+  const int turnTime = 250;
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH); 
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(PWM_R, 200);
+  analogWrite(PWM_L, 200);
+  delay(turnTime);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW); 
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(PWM_R, 0);
+  analogWrite(PWM_L, 0);
 }
 
 int acquireSensor(int pin) {
   int sum = 0;
-  int numSamples = 20; //TODO: What is this actually supposed to be?
+  int numSamples = 600;
   for (int i = 0; i < numSamples; i++) {
     sum += analogRead(pin);
   }
